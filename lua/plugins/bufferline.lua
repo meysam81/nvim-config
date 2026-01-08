4c64aafead956704e1b1d468c5cca3f8860fdad4
Meysam Azad <meysam@meysam.io>
2026-01-08 19:25:03 +0700

    feat: add keymap for close other tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -15,11 +15,14 @@ return {
 
     vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
     vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>x", ":bd<CR>", { silent = true })  -- close buffer
+    vim.keymap.set("n", "<leader>x", ":bd<CR>", { silent = true }) -- close buffer
     vim.keymap.set("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", { silent = true })
     vim.keymap.set("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", { silent = true })
     vim.keymap.set("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", { silent = true })
     vim.keymap.set("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", { silent = true })
     vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>xo", ":BufferLineCloseOthers<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>xl", ":BufferLineCloseLeft<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>xr", ":BufferLineCloseRight<CR>", { silent = true })
   end,
 }
